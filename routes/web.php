35170b15c25778d0fa64f3b449bb2390ea0e4a87
Widiasa Mahendra <dmforaname@gmail.com>
2022-06-05 17:29:29 +0700

    fix register route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Auth;
 Route::get('/login', [Auth\LoginController::class, 'index'])->name('login');
 Route::get('/register', [Auth\LoginController::class, 'register']);
 Route::post('/register' , [Auth\LoginController::class, 'registerStore'])->name('Register.store');
-Route::post('/register' , [Auth\LoginController::class, 'login'])->name('Login.store');
+Route::post('/login' , [Auth\LoginController::class, 'login'])->name('Login.store');
 Route::get('/logout' , [Auth\LoginController::class, 'logout'])->name('userLogout');
 Route::get('/user/get-token', [Auth\LoginController::class, 'getToken'])->middleware('auth');
 
